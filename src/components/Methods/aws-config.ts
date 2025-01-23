640966754539414948bf6266478fddb5da912477
raghavtraj <raghav.raj@experionglobal.com>
2025-01-23 10:13:06 +0530

    removed config

--- src/components/Methods/aws-config.ts
+++ src/components/Methods/aws-config.ts
@@ -3,7 +3,7 @@ import { S3Client } from "@aws-sdk/client-s3";
 export const s3Client = new S3Client({
     region: "us-west-2", // e.g., "us-east-1"
     credentials: {
-      accessKeyId: "AKIAR7HWX6UDFDHK4B6J",
-      secretAccessKey: "pcnUcl9N96aKEvcCvhyqB/6mNZ4QxpyXQ2ZqNMl2",
+      accessKeyId: "",
+      secretAccessKey: "",
     },
 });
